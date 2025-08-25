322aeb147d64b7a37cbdfb8ac89730926ec48ccf
matterofact <mattosborn85@protonmail.com>
2025-08-25 18:07:56 +0100

    implemented a few of the first row of opcodes where they are easily implemented, e.g inc, dec, etc, and created stubs for some of those which are not yet implemented. Stubbed memory map for vram and io.c

---
+++ io.c
@@ -0,0 +1,12 @@
+// $FF00		DMG	Joypad input
+// $FF01	$FF02	DMG	Serial transfer
+// $FF04	$FF07	DMG	Timer and divider
+// $FF0F		DMG	Interrupts
+// $FF10	$FF26	DMG	Audio
+// $FF30	$FF3F	DMG	Wave pattern
+// $FF40	$FF4B	DMG	LCD Control, Status, Position, Scrolling, and Palettes
+// $FF4F		CGB	VRAM Bank Select
+// $FF50		DMG	Boot ROM mapping control
+// $FF51	$FF55	CGB	VRAM DMA
+// $FF68	$FF6B	CGB	BG / OBJ Palettes
+// $FF70		CGB	WRAM Bank Select
