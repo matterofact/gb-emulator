614a2d57f23f21e21d009a91815a6f29636aac01
matterofact <mattosborn85@protonmail.com>
2025-08-25 16:51:03 +0100

    current stuff

--- main.c
+++ main.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 #include "cpu.h"
 #include "memory.h"
 
 
-int main() {
+int main(int argc, char *argv[]) {
+	char* rom_name = argv[1];
 	CPU cpu;
 	cpu_init(&cpu); //Game Boy starts exec here
 	memory_init();
-	
+//	load_rom();
+	 
 	for (int i = 0; i < 5; i++) {
 		printf("PC = %04X\n", cpu.PC);
 		cpu_step(&cpu);
 	}
 
 	return 0;
+}
+
+bool load_rom(const char *rom_name) {
+	FILE *rom = fopen(rom_name, "rb");
+	if (!rom) return false;
+
+	fseek(rom, 0, SEEK_END);
+	size_t rom_size = ftell(rom);
+	fseek(rom, 0, SEEK_SET);
+
+	fread(rom_bank0, 1, rom_size < 0x4000 ? rom_size : 0x4000, rom);
+
+	fclose(rom);
+	return true;
 }
--- memory.h
+++ memory.h
@@ -4,6 +4,19 @@
 #include <stdint.h>
 
 
+// ROM Banks
+
+extern uint8_t oam[0xA0];       // 160 bytes
+extern uint8_t unusable[0x60];  // 96 bytes
+extern uint8_t io[0x80];        // 128 bytes
+extern uint8_t hram[0x7F];      // 127 bytes
+extern uint8_t ie;              // 1 byte
+extern uint8_t oam[0xA0];       // 160 bytes
+extern uint8_t unusable[0x60];  // 96 bytes
+extern uint8_t io[0x80];        // 128 bytes
+extern uint8_t hram[0x7F];      // 127 bytes
+extern uint8_t ie;              // 1 byte
+
 // Functions
 
 uint8_t read8(uint16_t address);
